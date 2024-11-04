32ae10ce86f3d5ae612b27c9825bb5d216f3717b
Jessy DROUIN <jessy.drouin@equasens.com>
2024-11-04 16:30:50 +0100

    Player class creation

---
+++ snake_game/Player.h
@@ -0,0 +1,16 @@
+//
+//  Player.h
+//  snake_game
+//
+//  Created by Jessy Drouin on 04/11/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Player : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ snake_game/Player.m
@@ -0,0 +1,12 @@
+//
+//  Player.m
+//  snake_game
+//
+//  Created by Jessy Drouin on 04/11/2024.
+//
+
+#import "Player.h"
+
+@implementation Player
+
+@end
